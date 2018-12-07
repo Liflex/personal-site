70d500dbff3c4bf8ca14f6741fa63b3fab9ddf48
Arthur <a.dmitartur@opinionsn.com>
2018-12-07 17:33:53 +0300

    s

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,2 +1,28 @@
+var acceleration = 4000;
 
-$( "#test" ).draggable();
+// $(function () {
+//     while(true){
+//         $(this).delay(1000);
+//         if(acceleration !== 100) {
+//             acceleration = acceleration - 100;
+//         }
+//     }
+// });
+
+// getAcceleration = $(function () {
+//    return acceleration;
+// });
+//
+// setAcceleration = $(function (e) {
+//     acceleration = e;
+// });
+
+$(function () {
+    $("#test").draggable({
+        revert: true,
+        revertDuration: acceleration
+    })
+        .bind("dragstart", function (event, ui) {
+
+        });
+});
